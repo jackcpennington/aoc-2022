85fa049478163e0d8672960e55c8a0ee890b7636
sharmaondra <jackcpennigton@gmail.com>
2022-12-01 09:52:23 +0000

    clean input

--- day1/day1.py
+++ day1/day1.py
@@ -2,6 +2,6 @@ with open("day1/input.txt", 'r') as f:
     nums = [[int(num) for num in group.split("\n")]  for group in f.read().split("\n\n")]
 
 part1 = max(map(sum, nums))
-part2 = sum(sorted(map(sum, nums), reverse=True)[:3])
+part2 = sum(sorted(map(sum, nums))[-3:])
 
 print(part1, part2)
