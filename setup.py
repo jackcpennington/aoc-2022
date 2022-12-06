13fb1c627f1e482afa59f98d2b7376e1a4605e87
sharmaondra <jackcpennigton@gmail.com>
2022-12-06 11:27:58 +0000

    cleaned setup

--- setup.py
+++ setup.py
@@ -5,12 +5,10 @@ import requests
 
 
 def main(day):
-    
+
     with open("session_cookie.txt",'r') as f:
         cookie = f.readline()
-    
-    print(cookie)
-    
+
     headers = {'session': cookie}
     url = "https://adventofcode.com/2022/day/" + day + "/input"
 
@@ -19,10 +17,6 @@ def main(day):
 
     with open("day" + day + "/input.txt", 'w') as f:
         f.write(resp.text)
-        
-    print(day)
-
-
 
 if __name__ == "__main__":
     main(sys.argv[1])
